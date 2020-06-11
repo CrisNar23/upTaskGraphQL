d75064e8c959cc43621f4a62277dc152485db1ca
CrisNar23 <cristian_naranjo_15@hotmail.com>
2020-06-10 23:00:13 -0500

    Avance hasta 219 crear JWT

---
+++ db/resolvers.js
@@ -0,0 +1,68 @@
+const Usuario = require('../models/Usuario')
+const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+require('dotenv').config({ path: 'variables.env' })
+
+//Crea y firma un JWT
+const crearToken = (usuario, secreta, expiresIn) => {
+  const { id, email } = usuario
+
+  return jwt.sign({ id, email }, secreta, { expiresIn })
+}
+
+const resolvers = {
+  Query: {
+
+  },
+  Mutation: {
+    crearUsuario: async (_, { input }) => {
+      const { email, password } = input
+
+      const existeUsuario = await Usuario.findOne({ email })
+
+      //Si existe el usuario
+      if (existeUsuario) {
+        throw new Error('El usuario ya está registrado')
+      }
+
+      try {
+        //Hashear password
+        const salt = await bcryptjs.genSalt(10)
+        input.password = await bcryptjs.hash(password, salt)
+
+        //Registrar nuevo usuario
+        const nuevoUsuario = new Usuario(input)
+        nuevoUsuario.save()
+
+        return "Usuario creado correctamente"
+      }
+      catch (error) {
+        console.log(error)
+      }
+    },
+    autenticarUsuario: async (_, { input }) => {
+      const { email, password } = input
+
+      //Si el usuario existe
+      const existeUsuario = await Usuario.findOne({ email })
+
+      if (!existeUsuario) {
+        throw new Error('El usuario no existe')
+      }
+
+      //Si el password es correcto
+      const passwordCorrecto = await bcryptjs.compare(password, existeUsuario.password)
+
+      if (!passwordCorrecto) {
+        throw new Error('Password Incorrecto')
+      }
+
+      //Permitir acceso a la app
+      return {
+        token: crearToken(existeUsuario, process.env.SECRETA, '2hr')
+      }
+    }
+  }
+}
+
+module.exports = resolvers
